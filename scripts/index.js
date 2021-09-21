b3498394045add11fb18455c09fd2064619ec3f9
Saucedechoux <saucedechoux@gmail.com>
2021-09-21 18:50:43 +0200

    Added loadJson function and integrated it in code

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
+import { loadJson } from './fetchJson.js';
 import Tile from './Tile.js';
 import utils, { $ } from './utils.js';
 
 const map = $('#map');
-const mapContent = [0, 1, 2, 2, 2, 3, 4];
+const data = await loadJson() // Charge le JSON
+const mapContent = data.map[0].tiles[2]; // Récupère la première ligne du tableau 
+// const mapContent = [0, 1, 2, 2, 2, 3, 4];
 
 // Génère un tableau contenant toutes les cases (HTMLDivElement)
 const tiles = mapContent.map((typeTile) => new Tile({ type: typeTile }));
